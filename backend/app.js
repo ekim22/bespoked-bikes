9dfa2f04b8592a0969877a63a275d4d480ab1040
ekim22 <ekim2@ggc.edu>
2022-05-20 16:55:49 -0400

    init node server and db connection

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+
+const app = express();
+
+// DB address
+let mongoDB;
+if (app.settings.env === 'development') {
+  mongoDB = process.env.DB_LOCAL;
+} else {
+  mongoDB = process.env.DB_PROD;
+}
+
+// Connect to DB
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+mongoose.connect(mongoDB, options);
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
+mongoose.connection.once('open', () => {
+  console.log('Connected to database!');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const app = require('./backend/app');
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    console.log(error);
+    throw error;
+  }
+}
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe" + addr : "port " + port;
+  console.log("Listening on " + bind)
+}
+
+const port = process.env.PORT || "3000"
+app.set('port', port);
+
+const server = http.createServer(app);
+server.on("error", onError)
+server.on("listening", onListening)
+server.listen(port);
