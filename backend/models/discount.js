710ee6dc55608bbac335cd41b6a2a07a89872051
ekim22 <ekim2@ggc.edu>
2022-05-20 21:17:31 -0400

    fixed missing discountPercentage field in discount schema

--- backend/models/discount.js
+++ backend/models/discount.js
@@ -5,6 +5,7 @@ const discountSchema = mongoose.Schema({
   productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
   startDate: {type: Date, required: true},
   endDate: {type: Date, required: true},
+  discountPercentage: {type: Number, required: true},
 });
 
 module.exports = mongoose.model('Discount', discountSchema);
