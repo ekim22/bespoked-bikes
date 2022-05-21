3a5e1be97f4a4882ba54693737be4507732f507b
ekim22 <ekim2@ggc.edu>
2022-05-21 12:08:03 -0400

    created salesperson, product, customer, and sales components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalespersonComponent } from './salesperson/salesperson.component';
+import { ProductComponent } from './product/product.component';
+import { CustomerComponent } from './customer/customer.component';
+import { SalesComponent } from './sales/sales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalespersonComponent,
+    ProductComponent,
+    CustomerComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
