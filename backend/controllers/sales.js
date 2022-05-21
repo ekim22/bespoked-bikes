5871e5f1a0b20ff6cb04898f8838112ed2e97827
ekim22 <ekim2@ggc.edu>
2022-05-20 21:59:39 -0400

    added getSales and createSale routes and controllers

---
+++ backend/controllers/sales.js
@@ -0,0 +1,36 @@
+const Sales = require('../models/sales');
+
+module.exports.getSales = async (req, res) => {
+  try {
+    const sales = Sales.find().lean();
+    res.status(200).json({
+      message: 'Fetched sales.',
+      sales: sales,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Server failed to fetch sales people.',
+    });
+  }
+};
+
+module.exports.createSale = async (req, res) => {
+  try {
+    const newSale = {
+      productId: req.body.productId,
+      salespersonId: req.body.salespersonId,
+      customerId: req.body.customerId,
+      saleDate: new Date(req.body.saleDate),
+    };
+    await Sales.create(newSale);
+    res.status(201).json({
+      message: 'Sale successfully added to sales.',
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Server failed to create new sale.',
+    });
+  }
+};
--- backend/routes/sales.js
+++ backend/routes/sales.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 const salesController = require('../controllers/sales');
 
+router.get('', salesController.getSales);
+router.post('/create', salesController.createSale);
 
 module.exports = router;
