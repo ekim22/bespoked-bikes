9978caa1f4919a2838a2a0fcaf89b6418dfd8fa5
ekim22 <ekim2@ggc.edu>
2022-05-20 20:00:58 -0400

    created relevant route files for data models

---
+++ backend/routes/discount.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const discountController = require('../controllers/discount');
+
+
+module.exports = router;
---
+++ backend/routes/sales.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const salesController = require('../controllers/sales');
+
+
+module.exports = router;
