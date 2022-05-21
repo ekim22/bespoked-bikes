ae166bddfa4faeb4b1604b1204a37700bb6b9329
ekim22 <ekim2@ggc.edu>
2022-05-21 14:38:10 -0400

    registered salespeople, products, customers, sales routes in Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SalespersonComponent} from "./salesperson/salesperson.component";
+import {ProductComponent} from "./product/product.component";
+import {CustomerComponent} from "./customer/customer.component";
+import {SalesComponent} from "./sales/sales.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'salespeople', component: SalespersonComponent },
+  { path: 'products', component: ProductComponent },
+  { path: 'customers', component: CustomerComponent },
+  { path: 'sales', component: SalesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
