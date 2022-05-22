a5bb2e42462bdefb023e2ff9a329db63fdae0019
ekim22 <ekim2@ggc.edu>
2022-05-22 04:47:18 -0400

    removes "Id" portion from SalesModel attributes

--- src/app/features/sales/sales.model.ts
+++ src/app/features/sales/sales.model.ts
@@ -1,7 +1,11 @@
+import {ProductModel} from "../product/product.model";
+import {SalespersonModel} from "../salesperson/salesperson.model";
+import {CustomerModel} from "../customer/customer.model";
+
 export interface SalesModel {
   _id: string,
-  productId: string,
-  salespersonId: string,
-  customerId: string,
+  product: ProductModel,
+  salesperson: SalespersonModel,
+  customer: CustomerModel,
   saleDate: Date
 }
