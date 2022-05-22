cd9a1024073f70082b5c89f1f3fc2fc24b3bc5b1
ekim22 <ekim2@ggc.edu>
2022-05-21 22:03:56 -0400

    get customers

--- src/app/features/customer/customer.component.ts
+++ src/app/features/customer/customer.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {Observable} from "rxjs";
+import {CustomerModel} from "./customer.model";
+import {CustomerService} from "./customer.service";
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class CustomerComponent implements OnInit {
+  customers$!: Observable<CustomerModel[]>;
 
-  constructor() { }
+  // Table variables
+  displayedColumns = ['First Name', 'Last Name', 'Address', 'Phone', 'Start Date'];
+  expandedRow!: CustomerModel | null;
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
+    this.customerService.getCustomers();
+    this.customers$ = this.customerService.customers;
   }
 
 }
--- src/app/features/customer/customer.service.ts
+++ src/app/features/customer/customer.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {BehaviorSubject} from "rxjs";
+import {CustomerModel} from "./customer.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private readonly customers$: BehaviorSubject<CustomerModel[]> = new BehaviorSubject<CustomerModel[]>([]);
 
   constructor(private httpClient: HttpClient) { }
 
-  // TODO get list of customers
-  getCustomers() {}
+  getCustomers() {
+    this.httpClient.get<{ message: string, customers: CustomerModel[] }>(environment.apiUrl + 'customers')
+      .subscribe(res => {
+        this.customers$.next(res.customers);
+        console.log(res.message);
+      });
+  }
+
+  get customers() {
+    return this.customers$.asObservable();
+  }
 
 }
