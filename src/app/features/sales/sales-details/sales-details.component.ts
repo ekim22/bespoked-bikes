e5135a686248ad5cc20b3184b3834153149ee70e
ekim22 <ekim2@ggc.edu>
2022-05-22 04:49:45 -0400

    displays a sale in detail form

---
+++ src/app/features/sales/sales-details/sales-details.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import {SalesModel} from "../sales.model";
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-sales-details',
+  templateUrl: './sales-details.component.html',
+  styleUrls: ['./sales-details.component.css']
+})
+export class SalesDetailsComponent implements OnInit {
+  saleDetails!: SalesModel;
+  productDetails!: FormGroup;
+  customerDetails!: FormGroup;
+  salespersonDetails!: FormGroup;
+  saleDate: FormControl = new FormControl(new Date());
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (history.state['sale']) {
+      this.saleDetails = history.state['sale'];
+      this.productDetails = new FormGroup({
+        name: new FormControl(this.saleDetails.product.name),
+        manufacturer: new FormControl(this.saleDetails.product.manufacturer),
+        style: new FormControl(this.saleDetails.product.style),
+        purchasePrice: new FormControl(this.saleDetails.product.purchasePrice),
+        salePrice: new FormControl(this.saleDetails.product.salePrice),
+        qtyOnHand: new FormControl(this.saleDetails.product.qtyOnHand),
+        commissionPercentage: new FormControl(this.saleDetails.product.commissionPercentage),
+      });
+      this.customerDetails = new FormGroup({
+        firstName: new FormControl(this.saleDetails.customer.firstName),
+        lastName: new FormControl(this.saleDetails.customer.lastName),
+        address: new FormControl(this.saleDetails.customer.address),
+        phone: new FormControl(this.saleDetails.customer.phone),
+        startDate: new FormControl(this.saleDetails.customer.startDate)
+      });
+      this.salespersonDetails = new FormGroup({
+        firstName: new FormControl(this.saleDetails.salesperson.firstName),
+        lastName: new FormControl(this.saleDetails.salesperson.lastName),
+        address: new FormControl(this.saleDetails.salesperson.address),
+        phone: new FormControl(this.saleDetails.salesperson.phone),
+        startDate: new FormControl(this.saleDetails.salesperson.startDate),
+        terminationDate: new FormControl(this.saleDetails.salesperson.terminationDate),
+        manager: new FormControl(this.saleDetails.salesperson.manager)
+      });
+      this.saleDate = new FormControl({value: new Date(this.saleDetails.saleDate), disabled: true});
+    }
+  }
+
+}
