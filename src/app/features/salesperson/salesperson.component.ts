554951816f27eb17e0bf59edf47efa1bccbbc4c9
ekim22 <ekim2@ggc.edu>
2022-05-21 20:24:37 -0400

    display list of salespeople

--- src/app/features/salesperson/salesperson.component.ts
+++ src/app/features/salesperson/salesperson.component.ts
@@ -2,20 +2,37 @@ import {Component, OnInit} from '@angular/core';
 import {SalespersonService} from "./salesperson.service";
 import {SalespersonModel} from "./salesperson.model";
 import {Observable} from "rxjs";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-salesperson',
   templateUrl: './salesperson.component.html',
-  styleUrls: ['./salesperson.component.css']
+  styleUrls: ['./salesperson.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class SalespersonComponent implements OnInit {
   salesPeople$!: Observable<SalespersonModel[]>
+
+  // Table variables
+  displayedColumns = ['First Name', 'Last Name', 'Address', 'Phone', 'Start Date', 'Termination Date', 'Manager'];
+  expandedRow!: SalespersonModel | null;
 
   constructor(private salespersonService: SalespersonService) { }
 
   ngOnInit(): void {
     this.salespersonService.getSalesPeopleList();
     this.salesPeople$ = this.salespersonService.salesPeople;
+  }
+
+  updateInfo(position: number, id: string, form: NgForm) {
+    this.salespersonService.updateSalesperson(position, id, form.value);
   }
 
 }
