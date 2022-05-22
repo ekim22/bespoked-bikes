9927656e4a058861239decc7357dd2b468aea64c
ekim22 <ekim2@ggc.edu>
2022-05-22 04:45:15 -0400

    registers salesDetails and createSales components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {SalespersonComponent} from "./features/salesperson/salesperson.component
 import {ProductComponent} from "./features/product/product.component";
 import {CustomerComponent} from "./features/customer/customer.component";
 import {SalesComponent} from "./features/sales/sales.component";
+import {SalesDetailsComponent} from "./features/sales/sales-details/sales-details.component";
+import {CreateSaleComponent} from "./features/sales/create-sale/create-sale.component";
 
 const routes: Routes = [
   { path: 'salespeople', component: SalespersonComponent },
   { path: 'products', component: ProductComponent },
   { path: 'customers', component: CustomerComponent },
   { path: 'sales', component: SalesComponent },
+  { path: 'sales/details', component: SalesDetailsComponent},
+  { path: 'sales/create', component: CreateSaleComponent},
 ];
 
 @NgModule({
