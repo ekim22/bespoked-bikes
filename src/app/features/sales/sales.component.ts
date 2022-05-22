dccaaf5fc5587cf11a92e7de387ab49ca5f0d116
ekim22 <ekim2@ggc.edu>
2022-05-22 04:48:28 -0400

    display sales and implements createSale

--- src/app/features/sales/sales.component.ts
+++ src/app/features/sales/sales.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {SalesService} from "./sales.service";
+import {Observable} from "rxjs";
+import {SalesModel} from "./sales.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sales',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales.component.css']
 })
 export class SalesComponent implements OnInit {
+  sales$!: Observable<SalesModel[]>;
 
-  constructor() { }
+  // Table variables
+  displayedColumns = ['Sale Date', 'Customer', 'Product', 'Salesperson', 'Price', 'Salesperson Commission' ];
+
+  constructor(private salesService: SalesService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.salesService.getSales();
+    this.sales$ = this.salesService.sales;
+  }
+
+  viewSale(sale: SalesModel) {
+    this.router.navigateByUrl('sales/details', {state: {sale: sale}});
   }
 
 }
--- src/app/features/sales/sales.service.ts
+++ src/app/features/sales/sales.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {BehaviorSubject} from "rxjs";
+import {SalesModel} from "./sales.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
+  private readonly sales$: BehaviorSubject<SalesModel[]> = new BehaviorSubject<SalesModel[]>([]);
 
   constructor(private httpClient: HttpClient) { }
 
-  // TODO get list of sales
-  getSales() {}
+  getSales() {
+    this.httpClient.get<{message: string, sales: SalesModel[]}>(environment.apiUrl + 'sales').subscribe(res => {
+      this.sales$.next(res.sales);
+      console.log(res.message);
+    })
+  }
 
-  // TODO create a new sale
-  createSale() {}
+  createSale(productId: string, customerId: string, salespersonId: string, saleDate: string) {
+    const newSale = {
+      product: productId,
+      customer: customerId,
+      salesperson: salespersonId,
+      saleDate: saleDate
+    };
+    this.httpClient.post<{message: string}>(environment.apiUrl + 'sales/create', newSale).subscribe(res => {
+      console.log(res.message)
+    })
+  }
+
+  get sales() {
+    return this.sales$.asObservable();
+  }
 
 }
