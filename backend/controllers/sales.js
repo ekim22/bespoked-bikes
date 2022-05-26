3f534f3bd057c4d353aac9d3c45a096ee658884e
ekim22 <ekim2@ggc.edu>
2022-05-26 13:04:49 -0400

    updates qty on create sale; add fields to new sale

--- backend/controllers/sales.js
+++ backend/controllers/sales.js
@@ -8,7 +8,6 @@ module.exports.getSales = async (req, res) => {
     const sales = await Sales.find()
         .populate('product salesperson customer')
         .lean();
-
     res.status(200).json({
       message: 'Fetched sales.',
       sales: sales,
@@ -23,23 +22,30 @@ module.exports.getSales = async (req, res) => {
 
 module.exports.createSale = async (req, res) => {
   try {
-    const product = await Product.findById(req.body.product);
+    const product = await Product.findById(req.body.product).then(
+        (product) => {
+          product.qtyOnHand -= 1;
+          return product.save().then(product);
+        },
+    );
     const customer = await Customer.findById(req.body.customer);
     const salesperson = await Salesperson.findById(req.body.salesperson);
-
     const newSale = {
       product: req.body.product,
       productName: product.name,
+      manufacturer: product.manufacturer,
+      style: product.style,
+      purchasePrice: product.purchasePrice,
+      salePrice: req.body.productPrice,
+      commissionPercentage: product.commissionPercentage,
+      salespersonCommission: product.salePrice * product.commissionPercentage,
+      qtyOnHand: product.qtyOnHand,
       salesperson: req.body.salesperson,
       salespersonName: salesperson.firstName + ' ' + salesperson.lastName,
       customer: req.body.customer,
       customerName: customer.firstName + ' ' + customer.lastName,
-      salePrice: product.salePrice,
-      commissionPercentage: product.commissionPercentage,
-      salespersonCommission: product.salePrice * product.commissionPercentage,
       saleDate: new Date(req.body.saleDate),
     };
-
     await Sales.create(newSale);
     res.status(201).json({
       message: 'Sale was successfully created.',
