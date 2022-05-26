08de2730d663053c715533b3176c8a6e1c6424b4
ekim22 <ekim2@ggc.edu>
2022-05-26 13:37:18 -0400

    changes and improves create sale page
    Some style improvements. Several selects are gated on condition of other
    selects having user selected values. Commissions now properly calculate
    whether a discount is applicable depending on the sale date selected,
    and update in real-time on a date change while a salesperson and product
    are selected. The form is reset on a create sale (except date).

--- src/app/features/sales/create-sale/create-sale.component.ts
+++ src/app/features/sales/create-sale/create-sale.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProductService} from "../../product/product.service";
 import {CustomerService} from "../../customer/customer.service";
 import {SalespersonService} from "../../salesperson/salesperson.service";
@@ -8,6 +8,8 @@ import {SalespersonModel} from "../../salesperson/salesperson.model";
 import {Observable} from "rxjs";
 import {SalesService} from "../sales.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {tap} from "rxjs/operators";
+import {MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-create-sale',
@@ -15,9 +17,18 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrls: ['./create-sale.component.css']
 })
 export class CreateSaleComponent implements OnInit {
+  @ViewChild('selectedManufacturer') manufacturer!: MatSelect;
+  @ViewChild('selectedProduct') product!: MatSelect;
+  @ViewChild('selectedCustomer') customer!: MatSelect;
+  @ViewChild('selectedSalesperson') salesperson!: MatSelect;
+
   products$!: Observable<ProductModel[]>;
   customers$!: Observable<CustomerModel[]>;
   salespersons$!: Observable<SalespersonModel[]>;
+
+  manufacturers: Set<{}> = new Set<{}>();
+
+  productPrice!: number;
 
   constructor(private productService: ProductService,
               private customerService: CustomerService,
@@ -30,13 +41,48 @@ export class CreateSaleComponent implements OnInit {
     this.customerService.getCustomers();
     this.salespersonService.getSalesPeople();
 
-    this.products$ = this.productService.products;
+    this.products$ = this.productService.products.pipe(
+      tap(p => {
+        p.forEach(v => {
+          this.manufacturers.add(v.manufacturer)
+        })
+      })
+    );
     this.customers$ = this.customerService.customers;
     this.salespersons$ = this.salespersonService.salesPeople;
   }
 
   createSale(saleDate: string, productId: string, customerId: string, salespersonId: string) {
-    this.salesService.createSale(productId, customerId, salespersonId, saleDate);
-    this._snackBar.open("Sale was created!", "OK")
+    this.salesService.createSale(productId, customerId, salespersonId, saleDate, this.productPrice);
+    this._snackBar.open("Sale was created!", "OK", {
+      duration: 2000
+    });
+    this.resetSaleForm();
+  }
+
+  calculateCommission(price: number, commissionPercentage: number) {
+    return "$" + (price * commissionPercentage).toFixed(2);
+  }
+
+  checkSaleForDiscount(discounted: boolean, discountStartDate: Date, discountEndDate: Date, saleDate: string): boolean {
+    const discountActive = !(!discounted || new Date(saleDate) < discountStartDate || new Date(saleDate) > discountEndDate);
+    if (discountActive) {
+      this.productPrice = this.product.value.discountedPrice;
+    } else {
+      this.productPrice = this.product.value.salePrice;
+    }
+    return discountActive;
+  }
+
+  recalculateCommission() {
+    // Note: I like that this works.
+    this.salesperson.value = this.salesperson.value;
+  }
+
+  resetSaleForm() {
+    this.manufacturer.value = null;
+    this.product.value = null;
+    this.customer.value = null;
+    this.salesperson.value = null;
   }
 }
