8dfdcfc7ddb5b63f7e498bad6a52099c61a0c023
ekim22 <ekim2@ggc.edu>
2022-06-06 15:12:57 -0400

    update readme

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from "@angular/animations";
 import {ProductService} from "./product.service";
 import {NgForm} from "@angular/forms";
